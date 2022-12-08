a1cc3288df9dfe0c0cfd96b67a4adaa1c340f061
Oluwafemi <ogundarearnold@gmail.com>
2022-12-08 08:39:34 +0100

    Sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,6 +1,8 @@
+
+
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
 
@@ -11,7 +13,7 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -20,7 +22,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
